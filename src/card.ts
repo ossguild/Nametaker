4e44b76cb7b9edf3e5cde4173ba61d7ab4a29522
ATLAS2002 <siddharth211102@gmail.com>
2023-10-30 17:34:18 +0530

    quotes expand when hovered

--- src/card.ts
+++ src/card.ts
@@ -3,10 +3,16 @@ import { Contributor } from "../types";
 import extractTwitterUsername from "./lib/extractTwitterUsername";
 import { onMouseEnter, onMouseLeave } from "./lib/cardEffect";
 
+const useShrinked = (word: string, limit: number): string => {
+  if (word.length <= limit) return `"` + word + `"`;
+  return `"` + word.substring(0, limit - 4) + `..."`;
+};
+
 const contributorCard = (contributor: Contributor) => {
+  const shrinkedQuote = useShrinked(contributor.favoriteQuote, 30);
   const cardElement = document.createElement("div");
   cardElement.innerHTML = `
-  <div class="${styles.card} bg-neutral-200 hover:shadow-lg flex px-5 py-6 w-fit items-center justify-start gap-4 rounded-xl transition">
+  <div class="${styles.card} bg-neutral-200 hover:shadow-lg flex w-max px-5 pr-10 py-6 items-center justify-start gap-4 rounded-xl transition">
     <div class="rounded-full bg-blend-overlay overflow-clip z-50 w-24 h-24 bg-[#f3f3f3] flex items-center justify-center">
       <p class="text-5xl transition duration-300 hover:scale-125">
         ${contributor.emoji}
@@ -15,8 +21,8 @@ const contributorCard = (contributor: Contributor) => {
     <div class='${styles.glow} glow'></div>
       <div class="z-50">
         <h4 class="text-2xl font-bold">${contributor.name}</h4>
-        <p class="text-gray-500 w-fit font-serif italic">
-          "${contributor.favoriteQuote}"
+        <p class="quote text-gray-500 font-serif italic">
+          ${shrinkedQuote}
         </p>
         <div class="text-blue-600 rounded-xl transition duration-300 w-fit px-2 mt-2 text-sm font-mono cursor-pointer hover:text-white hover:bg-blue-500 hover:shadow-md hover:shadow-blue-600">
           ${
@@ -27,13 +33,15 @@ const contributorCard = (contributor: Contributor) => {
         </div>
       </div>
   </div>`;
-
-  cardElement.addEventListener("mouseenter", (e) =>
-    onMouseEnter(e, cardElement)
-  );
-  cardElement.addEventListener("mouseleave", (e) =>
-    onMouseLeave(e, cardElement)
-  );
+  const quote = cardElement.querySelector(".quote");
+  cardElement.addEventListener("mouseenter", (e) => {
+    onMouseEnter(e, cardElement);
+    quote!.innerHTML = useShrinked(contributor.favoriteQuote, Infinity);
+  });
+  cardElement.addEventListener("mouseleave", (e) => {
+    onMouseLeave(e, cardElement);
+    quote!.innerHTML = shrinkedQuote;
+  });
 
   return cardElement;
 };
@@ -44,9 +52,7 @@ export default function generateContributorCards(
   const cardWrapper = document.createElement("div");
   cardWrapper.classList.add(
     styles["card-wrapper"],
-    ..."gap-6 p-10 max-w-screen-2xl mx-auto mt-24 flex flex-col md:flex-row flex-wrap".split(
-      " "
-    )
+    ..."gap-6 p-10 flex md:flex-row flex-wrap justify-center".split(" ")
   );
 
   contributors.forEach((contributor) => {
